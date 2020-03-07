eb081aeccf37425431293b86cc3f633d5008b838
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-03-08 00:06:58 +0100

    EnterNewIslandInteractory: Implemented Island generation and presentation.

--- src/org/ethasia/adventuregrid/interactors/EnterNewIslandInteractor.java
+++ src/org/ethasia/adventuregrid/interactors/EnterNewIslandInteractor.java
@@ -1,11 +1,20 @@
 package org.ethasia.adventuregrid.interactors;
 
+import org.ethasia.adventuregrid.core.CoreClassesFactory;
+import org.ethasia.adventuregrid.core.environment.Island;
+import org.ethasia.adventuregrid.core.input.IslandGenerator;
+import org.ethasia.adventuregrid.interactors.output.IslandPresenter;
+
 public class EnterNewIslandInteractor {
     
     //<editor-fold defaultstate="collapsed" desc="Methods">
     
     public void setupNewIsland() {
+        IslandGenerator islandGenerator = CoreClassesFactory.getInstance().createIslandGenerator();
+        IslandPresenter islandPresenter = IoAdaptersFactoryForInteractors.getInstance().createIslandPresenter();
         
+        Island island = islandGenerator.generateIsland(64);
+        islandPresenter.presentIsland(island);
     }
     
     //</editor-fold>
--- tests/org/ethasia/adventuregrid/interactors/tests/EnterNewIslandInteractorTest.java
+++ tests/org/ethasia/adventuregrid/interactors/tests/EnterNewIslandInteractorTest.java
@@ -1,8 +1,15 @@
 package org.ethasia.adventuregrid.interactors.tests;
 
+import org.ethasia.adventuregrid.core.CoreClassesFactory;
+import org.ethasia.adventuregrid.core.mocks.CoreClassesMocksFactory;
+import org.ethasia.adventuregrid.core.mocks.IslandGeneratorMock;
 import org.ethasia.adventuregrid.interactors.EnterNewIslandInteractor;
 import org.ethasia.adventuregrid.interactors.IoAdaptersFactoryForInteractors;
 import org.ethasia.adventuregrid.interactors.mocks.IoAdaptersMockFactoryForInteractors;
+import org.ethasia.adventuregrid.interactors.mocks.IslandPresenterMock;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -11,6 +18,7 @@ public class EnterNewIslandInteractorTest {
     
     @BeforeClass
     public static void setupDependencies() {
+        CoreClassesFactory.setInstance(new CoreClassesMocksFactory());
         IoAdaptersFactoryForInteractors.setInstance(new IoAdaptersMockFactoryForInteractors());
     }
     
@@ -18,5 +26,8 @@ public class EnterNewIslandInteractorTest {
     public void testSetupNewIsland_generatesRandomIslandAndPassesItToIslandPresenter() {
         EnterNewIslandInteractor testCandidate = new EnterNewIslandInteractor();
         testCandidate.setupNewIsland();
+        
+        assertThat(IslandGeneratorMock.getLastGenerateIslandCallParameter(), is(64));
+        assertThat(IslandPresenterMock.getLastPresentIslandCallParameter().getXzDimension(), is(64));
     }
 }
