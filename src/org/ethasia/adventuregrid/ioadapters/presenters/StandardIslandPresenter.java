26cc22eb11411dcbbe2d2457da4d1d709efb25da
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-03-14 19:39:04 +0100

    Started implementation of IslandPresenter.

--- src/org/ethasia/adventuregrid/ioadapters/presenters/StandardIslandPresenter.java
+++ src/org/ethasia/adventuregrid/ioadapters/presenters/StandardIslandPresenter.java
@@ -1,7 +1,13 @@
 package org.ethasia.adventuregrid.ioadapters.presenters;
 
+import java.util.LinkedList;
+import java.util.List;
+import org.ethasia.adventuregrid.core.environment.Block;
+import org.ethasia.adventuregrid.core.environment.BlockTypes;
 import org.ethasia.adventuregrid.core.environment.Island;
 import org.ethasia.adventuregrid.interactors.output.IslandPresenter;
+import org.ethasia.adventuregrid.ioadapters.presenters.chunks.VisualChunkData;
+import org.ethasia.adventuregrid.ioadapters.presenters.output.ChunkRenderer;
 
 public class StandardIslandPresenter implements IslandPresenter {
     
@@ -15,6 +21,38 @@ public class StandardIslandPresenter implements IslandPresenter {
     
     @Override
     public void presentIsland(Island island) {
+        VisualChunkData dataToRender = new VisualChunkData();
+        // StandardBlockVisualsBuilder blockVisualDataBuilder = new 
+        ChunkRenderer chunkRenderer = TechnicalsFactory.getInstance().getChunkRendererInstance();
+        
+        int chunksPerEdge = 0;
+        for (int i = 0; i * CHUNK_EDGE_LENGTH_IN_BLOCKS < island.getXzDimension(); i++) {
+            chunksPerEdge++;
+        }
+        
+        List<Block> blocksToBeRendered = new LinkedList<>();
+        
+        for (int i = 0; i < chunksPerEdge; i++) {
+            for (int j = 0; j < chunksPerEdge; j++) {
+                
+                blocksToBeRendered.clear();
+                
+                for (int x = CHUNK_EDGE_LENGTH_IN_BLOCKS * i; x < CHUNK_EDGE_LENGTH_IN_BLOCKS * (i + 1); x++) {
+                    for (int y = CHUNK_EDGE_LENGTH_IN_BLOCKS * j; y < CHUNK_EDGE_LENGTH_IN_BLOCKS * (j + 1); y++) {
+                        for (int z = 0; z < Island.HEIGHT_IN_BLOCKS; z++) {
+                            if (x < island.getXzDimension() && y < island.getXzDimension()) {
+                                if (island.getBlockAt(x, y, z).getBlockType() != BlockTypes.AIR) {
+                                    blocksToBeRendered.add(island.getBlockAt(x, y, z));
+                                }
+                            }    
+                        }
+                    }                    
+                }
+                
+                dataToRender.setUpWithNumberOfBlocksInChunk(blocksToBeRendered.size());
+                dataToRender.setWorldPosition(i, j);
+            }            
+        }
     }    
     
     //</editor-fold>    
--- tests/org/ethasia/adventuregrid/ioadapters/presenters/tests/StandardIslandPresenterTest.java
+++ tests/org/ethasia/adventuregrid/ioadapters/presenters/tests/StandardIslandPresenterTest.java
@@ -1,12 +1,39 @@
 package org.ethasia.adventuregrid.ioadapters.presenters.tests;
 
+import org.ethasia.adventuregrid.core.environment.Island;
+import org.ethasia.adventuregrid.core.environment.RockBlock;
 import org.ethasia.adventuregrid.ioadapters.presenters.StandardIslandPresenter;
+import org.ethasia.adventuregrid.ioadapters.presenters.TechnicalsFactory;
+import org.ethasia.adventuregrid.ioadapters.presenters.chunks.VisualChunkData;
+import org.ethasia.adventuregrid.ioadapters.presenters.mocks.ChunkRendererMock;
+import org.ethasia.adventuregrid.ioadapters.presenters.mocks.TechnicalsMockFactory;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class StandardIslandPresenterTest {
     
+    @BeforeClass
+    public static void initDependencies() {
+        TechnicalsFactory.setInstance(new TechnicalsMockFactory());
+    }
+    
     @Test
     public void testInit() {
+        Island toRender = new Island(64);
+        toRender.placeBlockAt(RockBlock.getInstance(), 15, 0, 15);
+        toRender.placeBlockAt(RockBlock.getInstance(), 16, 0, 15);
+        toRender.placeBlockAt(RockBlock.getInstance(), 15, 0, 16);
+        toRender.placeBlockAt(RockBlock.getInstance(), 16, 0, 16);
+        
         StandardIslandPresenter testCandidate = new StandardIslandPresenter();
+        testCandidate.presentIsland(toRender);
+        
+        VisualChunkData lastRenderedChunkData = ChunkRendererMock.getLastRenderChunkCallData();
+        
+        assertThat(lastRenderedChunkData.getWorldX(), is(1));
     }
 }
