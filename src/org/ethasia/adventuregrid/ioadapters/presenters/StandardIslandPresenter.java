c28a675d366bc3300834d50ad7c26f2e8a83a396
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-03-18 22:37:01 +0100

    StandardIslandPresenter refactoring first part: split present method into submethods.

--- src/org/ethasia/adventuregrid/ioadapters/presenters/StandardIslandPresenter.java
+++ src/org/ethasia/adventuregrid/ioadapters/presenters/StandardIslandPresenter.java
@@ -22,82 +22,111 @@ public class StandardIslandPresenter implements IslandPresenter {
     //<editor-fold defaultstate="collapsed" desc="IslandPresenter Overrides">
     
     @Override
-    public void presentIsland(Island island) {
-        ChunkRenderer chunkRenderer = TechnicalsFactory.getInstance().getChunkRendererInstance();
-        
-        int chunksPerEdge = 0;
-        for (int i = 0; i * CHUNK_EDGE_LENGTH_IN_BLOCKS < island.getXzDimension(); i++) {
-            chunksPerEdge++;
-        }
+    public void presentIsland(Island island) {        
+        int chunkCountPerIslandEdge = calculateChunkCountPerIslandEdge(island);
         
         List<float[]> floatBuffersOfBlocksInChunk = new LinkedList<>();
         List<int[]> intBuffersOfBlocksInChunk = new LinkedList<>();
         
-        for (int i = 0; i < chunksPerEdge; i++) {
-            for (int j = 0; j < chunksPerEdge; j++) {
+        for (int i = 0; i < chunkCountPerIslandEdge; i++) {
+            for (int j = 0; j < chunkCountPerIslandEdge; j++) {
                 
-                VisualChunkData dataToRender = new VisualChunkData();
-                floatBuffersOfBlocksInChunk.clear();
-                intBuffersOfBlocksInChunk.clear();
-                int currentBlockRenderIndex = 0;
-                
-                for (int x = CHUNK_EDGE_LENGTH_IN_BLOCKS * i; x < CHUNK_EDGE_LENGTH_IN_BLOCKS * (i + 1); x++) {
-                    for (int z = CHUNK_EDGE_LENGTH_IN_BLOCKS * j; z < CHUNK_EDGE_LENGTH_IN_BLOCKS * (j + 1); z++) {
-                        for (int y = 0; y < Island.HEIGHT_IN_BLOCKS; y++) {
-                            if (x < island.getXzDimension() && z < island.getXzDimension()) {
-                                if (island.getBlockAt(x, y, z).getBlockType() != BlockTypes.AIR) {                                    
-                                    int inChunkX = x - CHUNK_EDGE_LENGTH_IN_BLOCKS * i;
-                                    int inChunkZ = z - CHUNK_EDGE_LENGTH_IN_BLOCKS * j;
-                                    
-                                    Block currentBlock = island.getBlockAt(x, y, z);
-                                    BlockVisualsBuilder blockVisualsBuilder = BlockVisualsBuilder.fromBlockType(currentBlock.getBlockType());
-                                    blockVisualsBuilder.setBlockToCreateDataFrom(currentBlock)
-                                        .setChunkPositionX(inChunkX)
-                                        .setChunkPositionY(y)
-                                        .setChunkPositionZ(inChunkZ)
-                                        .setRenderIndexInChunk(currentBlockRenderIndex)
-                                        .setFrontFaceOfBlockIsCovered(island.blockFaceAtPositionIsHidden(BlockFaceDirections.FRONT, x, y, z))
-                                        .setRightFaceOfBlockIsCovered(island.blockFaceAtPositionIsHidden(BlockFaceDirections.RIGHT, x, y, z))
-                                        .setBackFaceOfBlockIsCovered(island.blockFaceAtPositionIsHidden(BlockFaceDirections.BACK, x, y, z))
-                                        .setLeftFaceOfBlockIsCovered(island.blockFaceAtPositionIsHidden(BlockFaceDirections.LEFT, x, y, z))
-                                        .setBottomFaceOfBlockIsCovered(island.blockFaceAtPositionIsHidden(BlockFaceDirections.BOTTOM, x, y, z))
-                                        .setTopFaceOfBlockIsCovered(island.blockFaceAtPositionIsHidden(BlockFaceDirections.TOP, x, y, z))
-                                        .build();
-                                    
-                                    if (blockVisualsBuilder.getShapeVertices().length > 0) {
-                                        floatBuffersOfBlocksInChunk.add(blockVisualsBuilder.getShapeVertices());
-                                        floatBuffersOfBlocksInChunk.add(blockVisualsBuilder.getShapeNormals());
-                                        floatBuffersOfBlocksInChunk.add(blockVisualsBuilder.getBlockUvCoordinates());
-                                        intBuffersOfBlocksInChunk.add(blockVisualsBuilder.getShapeIndices());
-                                    
-                                        currentBlockRenderIndex += blockVisualsBuilder.getAmountOfAddedIndices();                                        
-                                    }
-                                }
-                            }    
-                        }
-                    }                    
-                }
+                VisualChunkData chunkRenderData = clearRenderDataForNewChunk(floatBuffersOfBlocksInChunk, intBuffersOfBlocksInChunk);
+                fillRenderDataForOneChunk(i, j, island, floatBuffersOfBlocksInChunk, intBuffersOfBlocksInChunk);
                 
                 if (intBuffersOfBlocksInChunk.size() > 0) {
-                    dataToRender.setUpWithNumberOfBlocksInChunk(intBuffersOfBlocksInChunk.size());
-                    dataToRender.setWorldPosition(i, j);
-                
-                    for (int k = 0; k < floatBuffersOfBlocksInChunk.size(); k += 3) {
-                        dataToRender.addVerticesToTemporaryBuffer(floatBuffersOfBlocksInChunk.get(k));
-                        dataToRender.addNormalsToTemporaryBuffer(floatBuffersOfBlocksInChunk.get(k + 1));
-                        dataToRender.addUvCoordinatesToTemporaryBuffer(floatBuffersOfBlocksInChunk.get(k + 2));
-                    }
-                
-                    for (int k = 0; k < intBuffersOfBlocksInChunk.size(); k++) {
-                        dataToRender.addIndicesToTemporaryBuffer(intBuffersOfBlocksInChunk.get(k));
-                    }
-                
-                    dataToRender.buildChunkData();
-                    chunkRenderer.renderChunk(dataToRender);                    
+                    renderChunk(chunkRenderData, i, j, floatBuffersOfBlocksInChunk, intBuffersOfBlocksInChunk);
                 }
             }            
         }
-    }    
+    }  
+    
+    private int calculateChunkCountPerIslandEdge(Island island) {
+        int result = 0;
+        for (int i = 0; i * CHUNK_EDGE_LENGTH_IN_BLOCKS < island.getXzDimension(); i++) {
+            result++;
+        }  
+        
+        return result;
+    }
+    
+    private VisualChunkData clearRenderDataForNewChunk(List<float[]> floatBuffersOfBlocksInChunk, List<int[]> intBuffersOfBlocksInChunk) {
+        floatBuffersOfBlocksInChunk.clear();
+        intBuffersOfBlocksInChunk.clear(); 
+        
+        return new VisualChunkData();
+    }
+    
+    private void fillRenderDataForOneChunk(int i, int j, Island island, List<float[]> floatBuffersOfBlocksInChunk, List<int[]> intBuffersOfBlocksInChunk) {
+        int amountOfVerticesAdded = 0;
+        
+        for (int x = CHUNK_EDGE_LENGTH_IN_BLOCKS * i; x < CHUNK_EDGE_LENGTH_IN_BLOCKS * (i + 1); x++) {
+            for (int z = CHUNK_EDGE_LENGTH_IN_BLOCKS * j; z < CHUNK_EDGE_LENGTH_IN_BLOCKS * (j + 1); z++) {
+                for (int y = 0; y < Island.HEIGHT_IN_BLOCKS; y++) {
+                    if (x < island.getXzDimension() && z < island.getXzDimension()) {
+                        if (island.getBlockAt(x, y, z).getBlockType() != BlockTypes.AIR) {                                    
+                            int inChunkX = x - CHUNK_EDGE_LENGTH_IN_BLOCKS * i;
+                            int inChunkZ = z - CHUNK_EDGE_LENGTH_IN_BLOCKS * j;
+                                    
+                            BlockVisualsBuilder blockVisualsBuilder = buildBlockVisualsFromBlock(island, x, y, z, inChunkX, inChunkZ, amountOfVerticesAdded);
+                                    
+                            if (blockVisualsBuilder.getShapeVertices().length > 0) {
+                                fillBuffersWithVisualRenderData(floatBuffersOfBlocksInChunk, intBuffersOfBlocksInChunk, blockVisualsBuilder);
+                                    
+                                amountOfVerticesAdded += blockVisualsBuilder.getAmountOfAddedIndices();                                        
+                            }
+                        }
+                    }    
+                }
+            }                    
+        }        
+    }
+    
+    private BlockVisualsBuilder buildBlockVisualsFromBlock(Island island, int x, int y, int z, int inChunkX, int inChunkZ, int currentBlockRenderIndex) {
+        Block currentBlock = island.getBlockAt(x, y, z);
+        BlockVisualsBuilder blockVisualsBuilder = BlockVisualsBuilder.fromBlockType(currentBlock.getBlockType());
+        blockVisualsBuilder.setBlockToCreateDataFrom(currentBlock)
+            .setChunkPositionX(inChunkX)
+            .setChunkPositionY(y)
+            .setChunkPositionZ(inChunkZ)
+            .setRenderIndexInChunk(currentBlockRenderIndex)
+            .setFrontFaceOfBlockIsCovered(island.blockFaceAtPositionIsHidden(BlockFaceDirections.FRONT, x, y, z))
+            .setRightFaceOfBlockIsCovered(island.blockFaceAtPositionIsHidden(BlockFaceDirections.RIGHT, x, y, z))
+            .setBackFaceOfBlockIsCovered(island.blockFaceAtPositionIsHidden(BlockFaceDirections.BACK, x, y, z))
+            .setLeftFaceOfBlockIsCovered(island.blockFaceAtPositionIsHidden(BlockFaceDirections.LEFT, x, y, z))
+            .setBottomFaceOfBlockIsCovered(island.blockFaceAtPositionIsHidden(BlockFaceDirections.BOTTOM, x, y, z))
+            .setTopFaceOfBlockIsCovered(island.blockFaceAtPositionIsHidden(BlockFaceDirections.TOP, x, y, z))
+            .build();     
+        
+        return blockVisualsBuilder;
+    }
+    
+    private void fillBuffersWithVisualRenderData(List<float[]> floatBuffersOfBlocksInChunk,  List<int[]> intBuffersOfBlocksInChunk, BlockVisualsBuilder blockRenderDataBuilder) {
+        floatBuffersOfBlocksInChunk.add(blockRenderDataBuilder.getShapeVertices());
+        floatBuffersOfBlocksInChunk.add(blockRenderDataBuilder.getShapeNormals());
+        floatBuffersOfBlocksInChunk.add(blockRenderDataBuilder.getBlockUvCoordinates());
+        intBuffersOfBlocksInChunk.add(blockRenderDataBuilder.getShapeIndices());        
+    }
+    
+    private void renderChunk(VisualChunkData chunkRenderData, int chunkPosX, int chunkPosY, List<float[]> floatBuffersOfBlocksInChunk,  List<int[]> intBuffersOfBlocksInChunk) {
+        ChunkRenderer chunkRenderer = TechnicalsFactory.getInstance().getChunkRendererInstance();        
+        
+        chunkRenderData.setUpWithNumberOfBlocksInChunk(intBuffersOfBlocksInChunk.size());
+        chunkRenderData.setWorldPosition(chunkPosX, chunkPosY);
+                
+        for (int k = 0; k < floatBuffersOfBlocksInChunk.size(); k += 3) {
+            chunkRenderData.addVerticesToTemporaryBuffer(floatBuffersOfBlocksInChunk.get(k));
+            chunkRenderData.addNormalsToTemporaryBuffer(floatBuffersOfBlocksInChunk.get(k + 1));
+            chunkRenderData.addUvCoordinatesToTemporaryBuffer(floatBuffersOfBlocksInChunk.get(k + 2));
+        }
+                
+        for (int k = 0; k < intBuffersOfBlocksInChunk.size(); k++) {
+            chunkRenderData.addIndicesToTemporaryBuffer(intBuffersOfBlocksInChunk.get(k));
+        }
+                
+        chunkRenderData.buildChunkData();
+        chunkRenderer.renderChunk(chunkRenderData);          
+    }
     
     //</editor-fold>    
 }
