bf55f94fb65e24d5a55b53120d585a46c82e7ed3
TheYaumrDevR <shmuel.yosef.shekelfeld@gmail.com>
2020-04-06 21:26:12 +0200

    Added face hiding calculation for transparent blocks.

--- src/org/ethasia/adventuregrid/core/environment/TransparentBlockFaceHidingStrategy.java
+++ src/org/ethasia/adventuregrid/core/environment/TransparentBlockFaceHidingStrategy.java
@@ -1,5 +1,13 @@
 package org.ethasia.adventuregrid.core.environment;
 
-public class TransparentBlockFaceHidingStrategy {
+public class TransparentBlockFaceHidingStrategy implements BlockFaceHidingStrategy {
+
+    //<editor-fold defaultstate="collapsed" desc="Methods">
     
+    @Override
+    public boolean faceIsHidden(Block coveredBlock, Block coveringBlock, BlockFaceDirections blockFace) {
+        return coveringBlock.getBlockType() != BlockTypes.AIR;
+    }    
+    
+    //</editor-fold>    
 }
